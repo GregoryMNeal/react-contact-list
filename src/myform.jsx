dd174b9e04d0b59b6de81516e45774255b093b96
Gregory Neal <gnealsr@gmail.com>
2017-11-20 10:05:24 -0600

    Update myform and app

---
+++ src/myform.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+
+class MyForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <AppBar title="My Form" />
+    );
+  }
+}
